0f142dee3b644b1c69d00b64fb6bfbeb596c205f
Barry <semieng94@gmail.com>
2020-03-13 21:10:11 -0500

    selector works

--- static/js/app.js
+++ static/js/app.js
@@ -1,29 +1,35 @@
-function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+function buildMetadata(sample) {
+  
+  d3.json(`/metadata/${sample}`).then((data) =>{
     // Use d3 to select the panel with id of `#sample-metadata`
-
+      var panel = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
-
+      panel.html("")
     // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+      Object.entries(data).forEach(([key,value])=>{
+        panel.append("h5").text(`${key}:${value}`)
+      })
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+  })
 }
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-
+  d3.json(`/samples/${sample}`).then((data)=>{
     // @TODO: Build a Bubble Chart using the sample data
-
+    const otu_ids = data.otu_ids
+    const otu_labels = data.otu_labels
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+  })
 }
 
 function init() {
